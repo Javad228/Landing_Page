350aa1e71521dc83e4edbd372e717d8026c3ac64
Javad228 <jbaghiro@purdue.edu>
2024-11-10 21:17:49 -0500

    added analytics

--- landing_workout/app/page.tsx
+++ landing_workout/app/page.tsx
@@ -81,7 +81,7 @@ export default function Component() {
   return (
     <div className="relative flex flex-col min-h-screen bg-gray-900 text-white">
       {/* Floating Phone Container */}
-      <div className="fixed bottom-10 right-10 w-[250px] h-[500px] z-50 hidden md:block">
+      <div className="fixed bottom-10 right-10 w-[250px] h-[500px] z-50 hidden md:block lg:w-[200px] lg:h-[400px]">
         {/* Phone Mockup */}
         <div className="relative w-full h-full bg-black rounded-[40px] border-8 border-gray-800 shadow-2xl">
           {/* Notch */}
@@ -130,6 +130,7 @@ export default function Component() {
 
       {/* Main content */}
       <main className="flex-1 mt-16">
+      <Analytics />
         {/* Hero Section */}
         <section className="relative w-full py-20 md:py-32 lg:py-48 bg-gradient-to-br from-purple-700 via-indigo-800 to-blue-900 overflow-hidden">
           {/* Background Particles */}
